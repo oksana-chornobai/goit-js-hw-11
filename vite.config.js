f8a7fc5adc2fbd960ba912345726e6568a84aebb
oksana-chornobai <chernobay.oks@icloud.com>
2025-08-25 09:56:44 +0300

    оновлення в package.json - build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/goit-js-hw-11/',
     build: {
       sourcemap: true,
       rollupOptions: {
